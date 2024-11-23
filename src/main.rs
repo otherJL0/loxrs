4aac2382a9a3c9bb720b4513f980cc4671ac9cec
Jonathan Lopez <jonathanglopez@gmail.com>
2024-11-23 08:09:08 -0500

    Set up simple repl and read file functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use reedline::{DefaultPrompt, Reedline, Signal};
+use reedline::{DefaultPrompt, DefaultPromptSegment, Reedline, Signal};
+use std::env;
 
-pub fn main() {
+fn repl() {
     let mut line_editor = Reedline::create();
-    let prompt = DefaultPrompt::default();
+    let prompt = DefaultPrompt::new(
+        DefaultPromptSegment::Basic("〈".to_string()),
+        DefaultPromptSegment::Empty,
+    );
 
     loop {
         let sig = line_editor.read_line(&prompt);
@@ -18,5 +22,18 @@ pub fn main() {
                 println!("Event: {:?}", x);
             }
         }
+    }
+}
+
+fn read_file(lox_file: &str) {
+    println!("Expected file: {lox_file}");
+}
+
+pub fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    match args.len() {
+        0 => repl(),
+        1 => read_file(&args[0]),
+        _ => eprintln!("Usage: loxrs PROGRAM.lox"),
     }
 }
